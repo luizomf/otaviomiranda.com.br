041fd3013c9f106cd1f9dc6c7a9b2e9915e8f170
Luiz Otávio <luizomf@gmail.com>
2025-06-24 19:53:29 -0300

    add generate_sitemap.py to commit.sh

--- commit.sh
+++ commit.sh
@@ -5,15 +5,31 @@ if [ ! "$#" -ge 1 ]; then
   exit 1
 fi
 
+
+
 echo "\n"
+echo "🐍 Executando \`python generate_sitemap.py\`"
+python generate_sitemap.py
+sleep 1
+
 echo "⚙️ Executando \`git add .\`"
 git add .
+sleep 1
+
 echo "⚙️ Executando \`git commit -m \"$1\"\`"
 git commit -m "$1"
+sleep 1
+
 echo "⚙️ Executando \`git add .\`"
 git add .
+sleep 1
+
 echo "⚙️ Executando \`git commit --amend --no-edit\`"
 git commit --amend --no-edit
+sleep 1
+
 echo "✅ Executando \`git push\`"
 git push
+sleep 1
+
 echo "\n"
