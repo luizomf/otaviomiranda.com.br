0e16c97306e01816fa0439cad63957575f9d69d5
Luiz Otávio <luizomf@gmail.com>
2025-06-25 13:49:48 -0300

    add new links to home

--- generate_sitemap.py
+++ generate_sitemap.py
@@ -1,19 +1,55 @@
 import os
+import re
 from datetime import datetime
 
 SITE_URL = "https://www.otaviomiranda.com.br"
 ROOT_DIR = "."  # raiz do seu projeto
-OUTPUT_FILE = "sitemap.xml"
+OUTPUT_SITEMAP_FILE = "sitemap.xml"
+OUTPUT_TITLES_FILE = "titles.html"
+
+
+TITLE_RE = re.compile(
+    r"<title[^>]*>(.*?)\s*[-|]\s*Otávio\s*Miranda\s*</title>",
+    re.IGNORECASE | re.DOTALL,
+)
+ignore_titles = ["_____CHANGE_ME_____"]
+ignore_paths = [
+    "./base/index.html",
+    "./2017/meus-cursos/",
+    "./2017/cursos-de-python-e-javascript-com-desconto/",
+]
 
 urls = []
 
 for root, dirs, files in os.walk(ROOT_DIR):
     if "index.html" in files:
         full_path = os.path.join(root, "index.html")
+        skip_loop = False
 
         # Ignorar se for raiz (index principal)
         if root == ".":
             continue
+
+        for ignore_path in ignore_paths:
+            if full_path.startswith(ignore_path):
+                print("🚫 PATH IGNORED:", full_path)
+                skip_loop = True
+                break
+
+        if skip_loop:
+            skip_loop = False
+            continue
+
+        with open(full_path, "r", encoding="utf8") as file:
+            html_content = file.read()
+            title = TITLE_RE.search(html_content)
+
+        if title:
+            title = title[1].strip()
+
+            if title in ignore_titles:
+                print("🚫 TITLE IGNORED:", title, full_path)
+                continue
 
         # Gera a URL relativa (ex: /2025/nome-do-video/)
         relative_url = os.path.relpath(root, ROOT_DIR).replace("\\", "/")
@@ -23,14 +59,17 @@ for root, dirs, files in os.walk(ROOT_DIR):
         lastmod_timestamp = os.path.getmtime(full_path)
         lastmod_date = datetime.fromtimestamp(lastmod_timestamp).date().isoformat()
 
-        urls.append((url, lastmod_date))
+        urls.append((url, lastmod_date, title, full_path, lastmod_timestamp))
+
 
 # Gerar sitemap
-with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+sorted_urls = sorted(urls, key=lambda item: item[4], reverse=True)
+
+with open(OUTPUT_SITEMAP_FILE, "w", encoding="utf-8") as f:
     f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
     f.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
 
-    for url, lastmod in sorted(urls):
+    for url, lastmod, *_ in sorted_urls:
         f.write("  <url>\n")
         f.write(f"    <loc>{url}</loc>\n")
         f.write(f"    <lastmod>{lastmod}</lastmod>\n")
@@ -38,4 +77,19 @@ with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
 
     f.write("</urlset>\n")
 
-print(f"✅ Sitemap gerado com {len(urls)} URLs em '{OUTPUT_FILE}'")
+
+with open(OUTPUT_TITLES_FILE, "w", encoding="utf-8") as f:
+    f.write("<!-- ARTICLE LINKS -->\n")
+    for url, lastmod, title, full_path, *_ in sorted_urls:
+        full_path = full_path[2:].replace("index.html", "")
+
+        f.write("<li>\n")
+        f.write(f'<a href="{full_path}">')
+        f.write(title)
+        f.write("</a>\n")
+        f.write("</li>\n")
+
+    f.write("<!-- END ARTICLE LINKS -->\n")
+
+
+print(f"✅ Sitemap gerado com {len(urls)} URLs em '{OUTPUT_SITEMAP_FILE}'")
