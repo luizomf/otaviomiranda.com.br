f5a33d8860723055d2a427e944d2c971f2123693
Luiz Otávio <luizomf@gmail.com>
2026-02-23 05:28:04 -0300

    feat(seo): implementar geração automática de sitemaps via @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.otaviomiranda.com.br',
   output: 'static',
+
   markdown: {
     shikiConfig: {
       // Tema que se assemelha ao legacy dark highlighter
@@ -11,4 +14,6 @@ export default defineConfig({
       wrap: true, // Força a quebra de linha em telas pequenas ao invés de scroll infinito
     },
   },
+
+  integrations: [sitemap()],
 });
